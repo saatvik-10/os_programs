be2fbf7ce32dfd4d562b72b1f43f567cdf102505
saatvik-10 <saatvikmadan529@gmail.com>
2025-08-05 12:17:07 +0530

    orphan process

---
+++ orphan_process.c
@@ -0,0 +1,24 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    pid_t pid = fork();
+
+    if(pid < 0) {
+        printf("Error in fork execution\n");
+        exit(EXIT_FAILURE);
+    } else {
+        if(pid == 0) {
+            // sleep(5);
+            printf("Child process with id %d\n", getpid());
+            printf("Child's parent process id is %d\n", getppid());
+            sleep(10);
+        } else {
+            printf("Parent process with id %d\n", getpid());
+            printf("Parent is terminating, child %d will become orphan\n", pid);
+        }
+    }
+    exit(0);
+}
