a11e7f1519afa6871bbd0f7e2a9155b3e876e9a5
saatvik-10 <saatvikmadan529@gmail.com>
2025-08-05 11:57:40 +0530

    basics of fork()

---
+++ os_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main () {
+    pid_t pid = fork();
+    
+    if (pid < 0) {
+        printf("Error in fork execution\n");
+    } else if (pid == 0) {
+        printf("Hello World from child process with id %d\n", getpid());
+    } else {
+        wait(NULL);
+        printf("Hello World from parent process with id %d\n", getpid());
+    }
+    return 0;
+}
